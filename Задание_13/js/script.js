77b6bad37064b0ba302de91c18f95d05563a345d
Alexey <dementyev15@mail.ru>
2019-06-03 17:05:54 +0300

    13

---
+++ Задание_13/js/script.js
@@ -0,0 +1,198 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    // timer
+
+    let deadline = '2019-06-10';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());
+        let seconds = Math.floor((t / 1000) % 60);
+        let minutes = Math.floor((t / 1000 / 60) % 60);
+        let hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function zero(number) {
+                if (number <= 9) {
+                    return '0' + number;
+                } else {
+                    return number;
+                }
+            }
+
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+
+    // modal
+
+    let more = document.querySelector('.more');
+    let overlay = document.querySelector('.overlay');
+    let close = document.querySelector('.popup-close');
+    let moreTab = document.querySelectorAll('.description-btn');
+    var t = this;
+
+    function modelWindow() {
+        overlay.style.display = 'block';
+        t.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    }
+
+    moreTab.forEach(function (item) {
+        item.addEventListener('click', function () {
+            modelWindow.call(this);
+        });
+    });
+
+    more.addEventListener('click', function () {
+        modelWindow.call(this);
+    });
+    
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.add('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    // Задание 12
+
+//     class Options {
+//         constructor(height, width, bg, fontSize, textAlign) {
+//             this.height = height;
+//             this.width = width;
+//             this.bg = bg;
+//             this.fontSize = fontSize;
+//             this.textAlign = textAlign;
+//         }
+//         Div() {
+//             let newDiv = document.createElement('div');
+//             document.body.appendChild(newDiv);
+//             let parametr = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+//             newDiv.style.cssText = parametr;
+//         }
+//     }
+//     let element = new Options(200, 350, "green", 20, "center");
+//     element.createDiv();
+// });
+
+
+// form
+
+
+
+     // Form
+
+     let message = {
+        loading: "load...",
+        success: 'thanks',
+        failure: "error"
+    };
+
+    let form = document.querySelector('.main-form');
+    let input = form.getElementsByTagName('input');
+    let statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value,key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj)
+        ;
+
+        request.send(json);
+
+
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+    
+
+    
+});
